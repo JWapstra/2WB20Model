7eec0ddf277a77953f3746b4bae223e3d37f1547
Lizard <41330651+Ben-0-mad@users.noreply.github.com>
2021-05-13 23:25:18 +0200

    Minor changes: added comments, uncommented print statement

--- basicmodel.py
+++ basicmodel.py
@@ -9,13 +9,13 @@ seed(10)
 class BasicModel:
     def __init__(self, prob_to_doc):
         self.state = [0 for i in range(0,22)]
-        self.NOPTS = 2 # number of optomologists
+        self.NOPTS = 3 # number of optomologists
 
         #Visitor probabilities 
         self.prob_1 = 0.3  #probability that 1 person enters building
         self.prob_2 = 0.1  #probability that 2 people enters building
         self.prob_emergency = 0.02 #1/50
-        self.prob_to_doc = prob_to_doc
+        self.prob_to_doc = prob_to_doc # the probability that somebody has to go to the doctor after visiting one of the assistents
 
         self.prob_skipsA2 = 0.5
         self.prob_skipsA4 = 0.5
@@ -25,6 +25,10 @@ class BasicModel:
         self.state = [0 for i in range(0,22)]
     
     def skipO2n(self, li: list, fromIndex):
+        """
+        This is a function to shorten the code in the 'moveOPeople' function.
+        It determines if a person can skip the O2n state and can skip to the O_(2n+1) state.
+        """
         for _ in range(li[fromIndex]):
             skipO = random() < self.prob_skipsO2n
             if skipO:
@@ -35,6 +39,9 @@ class BasicModel:
         return li
 
     def moveOPeople(self, li: list):
+        """
+        Determines what happens to the people who are at the optomologist when 5 minutes pass.
+        """
         busyOpts = sum(li[7:-2])
         freeOpts = self.NOPTS - busyOpts
 
@@ -77,9 +84,13 @@ class BasicModel:
 
 
     def moveAPeople(self, li: list):
-        # Assume that the waiting room and the optomologists have already been moved. 
-        # queue starts moving at the front (aka A5 moves first)
+        """
+        Determines what happens to people who are at one of the assistants.
         
+        We assume that the waiting room and the optomologists have already been handled.
+        The queue starts moving at the front (aka A5 moves first)
+        """
+
         # A5 moves to W1 or H
         for _ in range(li[5]):
             to_doc = random() < self.prob_to_doc
@@ -125,20 +136,26 @@ class BasicModel:
             self.state[0] = self.state[0] + 2
 
     def round(self):
+        """
+        5 minutes pass
+        """
         self.state = self.moveOPeople(self.state)
         self.state = self.moveAPeople(self.state)
         self.visitor()
         print(self.state)
 
     def run(self, t_max):
+        """
+        Run a simulation
+        """
         nr_waiting_patients = deque()
         for t in range(t_max):
             self.round()
             nr_waiting_patients.append(self.state[6])
-        print(nr_waiting_patients)
-        print("mean number of persons waiting: "+str(mean(nr_waiting_patients)))
+        # print(nr_waiting_patients)
+        print("mean number of people waiting: "+str(mean(nr_waiting_patients)))
 
 
 if __name__ == "__main__":
     BM = BasicModel(0.5)
-    BM.run(1000)
+    BM.run(10000)
